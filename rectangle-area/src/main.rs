c8574352be1d9a49c398ccaad5d28b0720a0e18f
NatProgramer <rodrigezsantiago40@gmail.com>
2023-12-29 16:09:36 -0500

    reactangle-area-calculator

--- rectangle-area/src/main.rs
+++ rectangle-area/src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    struct Rectangle { base: f32, height: f32 }
+
+    let rectangle1 = Rectangle {
+        base: 40.0,
+        height: 30.0,
+    };
+
+    println!("{}", find_rectangle_area(
+        rectangle1.base, 
+        rectangle1.height,
+        "m"
+    ));
+}
+
+fn find_rectangle_area(base: f32, height: f32, measure: &str) -> String {
+    let area: String = format!("{} {measure}", base * height);
+
+    area
 }
