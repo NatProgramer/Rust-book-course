ff69a23e3b0bae4aabe317451e54fc1a085f6404
NatProgramer <rodrigezsantiago40@gmail.com>
2024-02-29 18:46:30 -0500

    Upstream to ssh

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 
 #[derive(Debug)]
+=======
+#[derive(Debug)] // This trait make posible print a value of this enum to console with println
+#[warn(dead_code)]
+>>>>>>> 3cb3196 (Upstream to ssh)
 enum IpAddr {
     V4(String),
     V6(String),
@@ -7,6 +12,14 @@ enum IpAddr {
 
 fn main() {
     println!("Hello, world!");
+<<<<<<< HEAD
+=======
+
+    enum_basics()
+}
+
+fn enum_basics() {
+>>>>>>> 3cb3196 (Upstream to ssh)
     let home = IpAddr::V4(String::from("127.0.0.1"));
 
     let loopback = IpAddr::V6(String::from("::1"));
